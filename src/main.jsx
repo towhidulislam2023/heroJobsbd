3f39f053d0cfbfbd4a73a74f96204d710e8aa936
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-04-10 13:35:57 +0600

    main jsx correction

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom/client";
 import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import Home from './components/Home/Home';
 import JobDetails from './components/JobDetails/JobDetails';
@@ -43,16 +46,15 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
       },
       {
-        path: '*',
+        path: '/*',
         element: <ErrorPage />,
       }
     ]
   }
 ]);
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
